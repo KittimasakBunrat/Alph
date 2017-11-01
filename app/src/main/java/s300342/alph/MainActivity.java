85ff81b50973e6442575f8ac69f9ee62e5307fa5
Kittimasak Bunrat <kittimasak.bunrat@gmail.com>
2017-11-01 13:53:56 +0100

    - Added Permission for RECORD_AUDIO

--- app/src/main/java/s300342/alph/MainActivity.java
+++ app/src/main/java/s300342/alph/MainActivity.java
@@ -1,13 +1,21 @@
 package s300342.alph;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -20,13 +28,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},3);
+        }
+
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
         speechRecognizer.setRecognitionListener(new Listener());
 
         button = findViewById(R.id.button);
         text = findViewById(R.id.text);
 
+        button.setOnClickListener(this);
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case 3: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent i = new Intent(this, MainActivity.class);
+                    startActivity(i);
+                } else {
+                    finish();
+                }
+                finish();
+            }
+        }
     }
 
     @Override
@@ -34,11 +62,69 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
 
             case R.id.button:
-
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,5);
+                speechRecognizer.startListening(intent);
                 break;
 
             default:
                 break;
         }
     }
+
+    class Listener implements RecognitionListener {
+        @Override
+        public void onReadyForSpeech(Bundle bundle) {
+            text.setText("Ready for speech");
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+            text.setText("Beginning of speech");
+        }
+
+        @Override
+        public void onRmsChanged(float v) {
+            text.setText("onRmsChanged");
+        }
+
+        @Override
+        public void onBufferReceived(byte[] bytes) {
+            text.setText("onBufferReceived");
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            text.setText("onEndOfSpeech");
+        }
+
+        @Override
+        public void onError(int i) {
+            text.setText("onError");
+        }
+
+        @Override
+        public void onResults(Bundle bundle) {
+            String str = new String();
+            ArrayList data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            for (int i = 0; i < data.size(); i++)
+            {
+                str += data.get(i);
+            }
+            text.setText("results: "+ str);
+        }
+
+        @Override
+        public void onPartialResults(Bundle bundle) {
+
+        }
+
+        @Override
+        public void onEvent(int i, Bundle bundle) {
+
+        }
+    }
 }
+
+
